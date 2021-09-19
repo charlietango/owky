546e3e560037d308f5dde93c65861408a04c523a
Catalin Tudorache <catalin@appes.co>
2021-09-19 12:27:55 +0200

    Install and configure eslint

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App(): JSX.Element {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
