a3eaafa6a3197d8b9cb0f910090cbbe9b4cd3d42
Catalin Tudorache <catalin@appes.co>
2021-11-21 11:25:54 +0100

    Add persistence layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 import MainStackNavigator from '@navigation/MainStackNavigator';
 import { store } from '@store/store';
 
+const persistor = persistStore(store);
+
 export default function App(): JSX.Element {
   return (
     <Provider store={store}>
-      <StatusBar style={'auto'} />
-      <MainStackNavigator />
+      <PersistGate loading={null} persistor={persistor}>
+        <StatusBar style={'auto'} />
+        <MainStackNavigator />
+      </PersistGate>
     </Provider>
   );
 }
--- src/state/store/store.ts
+++ src/state/store/store.ts
@@ -1,10 +1,27 @@
-import uriReducer from '@slices/uri';
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import uriReducer from '@slices/uri';
+
+const reducers = combineReducers({
+  uri: uriReducer,
+});
+const persistedReducers = persistReducer({ key: 'root', storage: AsyncStorage }, reducers);
+
+/**
+ * https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
+ */
 const store = configureStore({
-  reducer: {
-    uri: uriReducer,
-  },
+  reducer: persistedReducers,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
