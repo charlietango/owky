bb74b4c19575e3f7a3425c1ecbefccc002920815
Catalin Tudorache <catalin@appes.co>
2021-12-23 18:57:50 +0100

    Add SearchInput component

---
+++ src/component/SearchInput/SearchInput.tsx
@@ -0,0 +1,13 @@
+import { TextInput } from 'react-native';
+import styled from 'styled-components';
+
+const SearchInput = styled(TextInput)`
+  background-color: #efefef;
+  margin: 4px 16px;
+  padding: 6px 8px;
+  border-radius: 6px;
+  font-size: 16px;
+  line-height: 18px;
+`;
+
+export default SearchInput;
