dbead5e193c6be28acbc3b17f70586565a511a00
Catalin Tudorache <catalin@appes.co>
2021-12-27 12:40:21 +0100

    Add switch component

---
+++ src/component/Switch/Switch.tsx
@@ -0,0 +1,32 @@
+import { View, Text, Switch as NativeSwitch } from 'react-native';
+import React from 'react';
+import styled from 'styled-components';
+
+type SwitchProps = {
+  label: string;
+  checked: boolean;
+  onChange: (value: boolean) => void;
+};
+
+const Container = styled(View)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom-width: 0.3px;
+  border-bottom-color: #c8c7cc;
+  padding: 8px 0;
+`;
+
+const Label = styled(Text)`
+  font-size: 16px;
+  line-height: 19px;
+`;
+
+export default function Switch(props: SwitchProps): JSX.Element {
+  return (
+    <Container>
+      <Label>{props.label}</Label>
+      <NativeSwitch value={props.checked} onValueChange={props.onChange} />
+    </Container>
+  );
+}
