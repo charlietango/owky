91f08af1908eca6519bbc34889f3756b4b3d020c
Catalin Tudorache <catalin@appes.co>
2022-01-14 15:43:52 +0100

    Further color adjustments

--- src/screen/LockScreen/LockScreen.tsx
+++ src/screen/LockScreen/LockScreen.tsx
@@ -3,7 +3,7 @@ import { Image, SafeAreaView } from 'react-native';
 
 export default function LockScreen(): JSX.Element {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#240046' }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#343A40' }}>
       <Image style={{ width: '100%', height: '100%' }} source={require('@assets/splash.png')} />
     </SafeAreaView>
   );
